f1af361b414f9d21d11af1230dc06f1a91e2288b
Janak Patel <71401432+jnkptl@users.noreply.github.com>
2020-10-25 16:29:51 +0200

    Update pt1.c

--- pt1.c
+++ pt1.c
@@ -4,9 +4,9 @@ int main()
 {
     long long int a;
     long long int b;
-    printf("type four hundred thousand as a number:\n");
+    printf("type four hundred thousand as a number: ");
     scanf("%lli",&a);
-    printf("type one hundred thousand as a number:\n");
+    printf("type one hundred thousand as a number: ");
     scanf("%lli",&b);
     long long int c = a * b;
     printf("%lli * %lli = %lli",a,b,c);
