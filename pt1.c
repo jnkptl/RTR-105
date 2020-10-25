12254d3b97cd9d90b4ed7afee25320d58cae21c6
Janak Patel <71401432+jnkptl@users.noreply.github.com>
2020-10-25 16:20:02 +0200

    Create pt1.c

---
+++ pt1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    long long int a;
+    long long int b;
+    printf("type four hundred thousand as a number:\n");
+    scanf("%lli",&a);
+    printf("type one hundred thousand as a number:\n");
+    scanf("%lli",&b);
+    long long int c = a * b;
+    printf("%lli * %lli = %lli",a,b,c);
+    
+    
+    return 0;
+}
