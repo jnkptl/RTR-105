610a1628bdc35d50b8d2f5cf298f8cd39c85bde7
Janak Patel <71401432+jnkptl@users.noreply.github.com>
2020-10-25 20:56:22 +0200

    Update pt2.c

--- pt2.c
+++ pt2.c
@@ -1,35 +1,40 @@
 #include <stdio.h>
-
+	
 int main()
-{
-    char a;
-//    printf("value of a: %d\n",a);
-    printf("type a single decmical value (0-9): ");
-    scanf("%c",&a);
-    char z = a >> 1; // 1 divides by 2 [2^1]
-    char y = z >> 1; // 2 divides by 4 [2^2]
-    char x = y >> 1; // 3 divides by 8 [2^3]
-    char w = x >> 1; // 4 divides by 16 [2^4]
+{	
+	char a;
+	//printf("value of a: %d\n",a);
+	printf("type a single decmical value (0-9): ");
+	scanf("%c",&a);
+	char z = a >> 1; // 1 divides by 2 [2^1]
+	char y = z >> 1; // 2 divides by 4 [2^2]
+	char x = y >> 1; // 3 divides by 8 [2^3]
+	char w = x >> 1; // 4 divides by 16 [2^4]
+	
 
-    char b = a - z - z; // remainder when a divided by 2
-    char c = z - y - y; // remainder when z divided by 2
-    char d = y - x - x; // remainder when y divided by 2
-    char e = x - w - w; // remainder when x divided by 2
-    //printf("result: z=%d, y=%d, x=%d, w=%d\n",z,y,x,w);
-    printf("binary value: %d%d%d%d\n",e,d,c,b);
-    /*
-    0 = 0000 = 48
-    1 = 0001 = 49
-    2 = 0010 = 50
-    3 = 0011 = 51
-    4 = 0100 = 52
-    5 = 0101 = 53
-    6 = 0110 = 54
-    7 = 0111 = 55
-    8 = 1000 = 56
-    9 = 1001 = 57
-    */
-    
-    
-    return 0;
+
+	char Z = z << 1;
+	char b = a - Z; // remainder when a divided by 2
+	char Y = y << 1;
+	char c = z - Y; // remainder when z divided by 2
+	char X = x << 1;
+	char d = y - X; // remainder when y divided by 2
+	char W = W << 1;
+	char e = x - W; // remainder when x divided by 2
+	//printf("result: z=%d, y=%d, x=%d, w=%d\n",z,y,x,w);
+	printf("binary value: %d%d%d%d\n",e,d,c,b);
+	/*
+	0 = 0000 = 48
+	1 = 0001 = 49
+	2 = 0010 = 50
+	3 = 0011 = 51
+	4 = 0100 = 52
+	5 = 0101 = 53
+	6 = 0110 = 54
+	7 = 0111 = 55
+	8 = 1000 = 56
+	9 = 1001 = 57
+	*/
+
+	return 0;
 }
