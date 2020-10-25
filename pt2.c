e381d4e728676605d462f19d76b5b0a2de1bd752
Janak Patel <71401432+jnkptl@users.noreply.github.com>
2020-10-25 20:33:34 +0200

    Update pt2.c

--- pt2.c
+++ pt2.c
@@ -6,30 +6,30 @@ int main()
 //    printf("value of a: %d\n",a);
     printf("type a single decmical value (0-9): ");
     scanf("%c",&a);
-    char z = a >> 1; // divides by 2 [2^1]
-    char y = z >> 2; // divides by 4 [2^2]
-    char x = y >> 3; // divides by 8 [2^3]
-    char w = x >> 4; // divides by 16 [2^4]
+    char z = a >> 1; // 1 divides by 2 [2^1]
+    char y = z >> 1; // 2 divides by 4 [2^2]
+    char x = y >> 1; // 3 divides by 8 [2^3]
+    char w = x >> 1; // 4 divides by 16 [2^4]
 
-    char b = a % 2; // remainder when a divided by 2
-    char c = z % 2; // remainder when a divided by 4
-    char d = x % 2; // remainder when a divided by 8
-    char e = y % 2; // remainder when a divided by 16
-    //printf("result: z=%d y=%d x=%d w=%d\n",z,y,x,w);
+    char b = a - z - z; // remainder when a divided by 2
+    char c = z - y - y; // remainder when z divided by 2
+    char d = y - x - x; // remainder when y divided by 2
+    char e = x - w - w; // remainder when x divided by 2
+    //printf("result: z=%d, y=%d, x=%d, w=%d\n",z,y,x,w);
     printf("binary value: %d%d%d%d",e,d,c,b);
     /*
-    verification:
-    0 = 0000; 0 = 48
-    1 = 0001; 1 = 49
-    2 = 0010; 2 = 50
-    3 = 0011; 3 = 51
-    4 = 0100; 4 = 52
-    5 = 0101; 5 = 53
-    6 = 0110; 6 = 54
-    7 = 0111; 7 = 55
-    8 = 1000; 8 = 56
-    9 = 1001; 9 = 57
+    0 = 0000 = 48
+    1 = 0001 = 49
+    2 = 0010 = 50
+    3 = 0011 = 51
+    4 = 0100 = 52
+    5 = 0101 = 53
+    6 = 0110 = 54
+    7 = 0111 = 55
+    8 = 1000 = 56
+    9 = 1001 = 57
     */
+    
     
     return 0;
 }
