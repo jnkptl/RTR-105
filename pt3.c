be68d15588291553ede00fa15ed8239db0662eeb
jnk <jnkptl23@gmail.com>
2020-10-29 14:24:20 +0200

    Programming Task 3

---
+++ pt3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int dec_1,dec_2,dec_3;
+	printf("enter three decimal numbers: ");
+	scanf("%i\n",&dec_1);
+	scanf("%i\n",&dec_2);
+	scanf("%i\n",&dec_3);
+
+	printf("%i\n",dec_1);
+	printf("%i\n",dec_2);
+	printf("%i\n",dec_3);
+
+	return 0;
+}
