9c91d6cbf375e8248b62da447287746dc812cffd
jnk <jnkptl23@gmail.com>
2021-01-10 23:40:30 +0000

    update

---
+++ works/4lw_integral/outline_task.c
@@ -0,0 +1,15 @@
+/*
+Write program so that it calculates and displays:
+- dialogue with user for obtaining value of a
+- dialogue with user for obtaining value of b
+- dialogue with user for obtaining value of precision
+- calculated value of area between f(x) and x-axis (using rectangle rule)
+- calculated value of area between f(x) and x-axis (using trapezoid rule)
+- calculated value of area between f(x) and x-axis (using Simpson's rule)
+
+- upload to github figure with shaded area between f(x) and x-axis in interval [a:b] // use gnuplot
+
+append report [nano works/4lw_derivative/README.md] // w image
+save history [history > history_yyyymmdd.txt]
+upload repository [./git-upload yyyymmdd_hh_mm]
+*/
